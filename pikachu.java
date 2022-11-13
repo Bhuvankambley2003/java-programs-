b790b451caf8df2ff162de8eba9aa9968deceb3c
bhuvan <pbhuvankambley2003@gmail.com>
2022-11-13 21:23:39 +0530

    pikachu.java

---
+++ pikachu.java
@@ -0,0 +1,9 @@
+public class pikachu {
+    public static void main(String [] args){
+
+        pokemon p1 = new pokemon("eveee",25);
+        System.out.println(p1.level);
+        p1.attack();
+
+    }
+}
